8d35e004a81504f172092e12c41e19ab4ccb293a
MARIKO\Curio <mariko.haruki.hal@gmail.com>
2026-04-09 01:24:21 +0900

    UI用のオブジェクトを追加　制作用のシーンを作成　プロトタイプのシーンをファイル移動

---
+++ Assets/User/Mariko/Script/AngelGageUI.cs
@@ -0,0 +1,33 @@
+// ---------------------------------------------------------
+// AngelGageUI.cs
+// 作成日:  2026/3/
+// 作成者:  
+// 概要:
+// ---------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AngelGageUI : MonoBehaviour
+{
+	// [SerializeField] private int _numId;
+
+
+	private void Awake()
+	{
+
+	}
+
+	private void Start() 
+	{
+
+	}
+	
+	private void Update() 
+	{
+
+	}
+}
