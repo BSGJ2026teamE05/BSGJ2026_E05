6335c3df0dead3fb03655a163fa60c0648571b8f
MARIKO\Curio <mariko.haruki.hal@gmail.com>
2026-04-09 01:43:08 +0900

    AngelGageUI.cs　使用するImageのメンバ変数を宣言

--- Assets/User/Mariko/Script/AngelGageUI.cs
+++ Assets/User/Mariko/Script/AngelGageUI.cs
@@ -1,27 +1,46 @@
 // ---------------------------------------------------------
 // AngelGageUI.cs
-// 作成日:  2026/3/
-// 作成者:  
-// 概要:
+// 作成日:  2026/4/9
+// 作成者:  Mariko Haruki
+// 概要:天使ゲージUI
 // ---------------------------------------------------------
 
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class AngelGageUI : MonoBehaviour
 {
-	// [SerializeField] private int _numId;
+    [SerializeField] private Image _frame;
+	public Image Frame => _frame;
 
+    [SerializeField] private Image _gazeUI;
+    public Image GageUI => _gazeUI;
 
-	private void Awake()
+    [SerializeField] private Image _overgazeUI;
+    public Image OverGageUI => _overgazeUI;
+
+    [SerializeField] private Image _wingUI;
+    public Image WingUI => _wingUI;
+
+    private void Awake()
 	{
 
 	}
 
 	private void Start() 
+	{
+        InitializeGageUI();
+	}
+
+    // =========================================================================
+    // 初期化関数
+    // =========================================================================
+    private void InitializeGageUI()
 	{
 
 	}
@@ -30,4 +49,6 @@ public class AngelGageUI : MonoBehaviour
 	{
 
 	}
+
+
 }
